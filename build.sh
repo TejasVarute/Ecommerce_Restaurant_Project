3ca7f988fc99679fcbc01f8ffc0e381424b23050
TejasVarute <tvarute@gmail.com>
2025-04-23 13:58:22 +0530

    update build

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python mange.py migrate
+python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
-    python mange.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
